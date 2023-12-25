1e2d54bf9ef81d388238fa0c478d8a94dbff7254
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2023-12-25 19:27:34 -0300

    Initial files

---
+++ src/main/java/combr/com/player/registration/playerregistration/PlayerRegistrationApplication.java
@@ -0,0 +1,13 @@
+package combr.com.player.registration.playerregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/combr/com/player/registration/playerregistration/PlayerRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package combr.com.player.registration.playerregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
